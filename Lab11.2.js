013c269296a3b8fb5f651212f295e66a3f15ffe3
StevenCude67 <124187339+StevenCude67@users.noreply.github.com>
2023-04-26 12:32:14 -0400

    Create Lab11.2.js

---
+++ Lab11.2.js
@@ -0,0 +1,92 @@
+/*
+1. Less Bland Graphics
+2. Better text
+3. Bigger sprites
+*/
+
+/*
+
+1. Create ship that moves back and forth
+2. Create squares that fall to the ground 
+3. Create bullets
+3. Create heart icons
+4. create title screen
+5. create game over screen
+
+*/
+
+function setup() {
+  createCanvas(400, 400);
+  
+  /*
+  draw ship
+  */
+  
+  /*
+  draw titleScreen
+  */
+  
+  /*
+  draw gameOver
+  */
+  
+  /*
+  square.show
+  square.move
+  */
+  
+  /*
+  bullet.show
+  bullet.move
+  */
+  
+  /*
+  heart.show
+  heart.move
+  */
+}
+
+function draw() {
+  background(220);
+}
+
+/*
+function ship(shipX, shipY){
+push();
+translate(shipX, shipY);
+scale(shipSize);
+rect(0, 0, shipSize);
+fill(red)
+rect(coordinates of smaller red rectangle)
+pop();
+}
+*/
+
+/*
+function titleScreen(){
+create font and coordinates for title screen that changes when a button is pressed
+}
+*/
+
+/*
+function gameOver(){
+shows up when players hearts run out
+}
+*/
+
+/*
+function keyPressed();
+when the left and right directional keys are pressed the spaceship moves in the Y direction
+*/
+
+/*
+Create a class that creates indiviual falling squares that interact with ship and bullets 
+*/
+
+/*
+Create a class that makes bullets that interact with falling squares
+*/
+
+/*
+Create a class that shopws health and each heart runs out when player gets hit or misses square, shows a skeleton if all run out
+*/
