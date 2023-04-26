111aa082b9e0c03917ab33731b28c3735c3a491a
StevenCude67 <124187339+StevenCude67@users.noreply.github.com>
2023-04-26 12:20:09 -0400

    Create Lab11.1.js

---
+++ Lab11.1.js
@@ -0,0 +1,59 @@
+/*
+1. Make moving more intuitive 
+2. Make a title screen
+3. Make a way to win
+*/
+
+/*
+
+1. Create triangle that can be moved with arrow keys
+2. Create astroids that break into smaller astroids when hit
+3. Create bullets
+
+*/
+
+function setup() {
+  createCanvas(400, 400);
+  
+  /*
+  draw spaceShip
+  */
+  
+  /*
+  astroid.show
+  astroid.move
+  */
+  
+  /*
+  bullet.show
+  bullet.move
+  */
+}
+
+function draw() {
+  background(220);
+}
+
+/*
+function spaceShip(shipX, shipY){
+push();
+translate(shipX, shipY);
+scale(shipSize);
+noStroke
+triangle(triangle cordinates);
+pop();
+}
+*/
+
+/*
+function keyPressed();
+when the four directional keys are pressed the spaceship moves in the X or Y direction
+*/
+
+/*
+Create a class that creates indiviual astroids
+*/
+
+/*
+Create a class that makes bullets
+*/
